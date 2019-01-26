709c4cb943ea98d24c672dc2216899f038788d82
Matt Passarelli <mattvpassarelli@gmail.com>
2019-01-26 15:44:45 -0500

    Added function to test for client connect

--- main.py
+++ main.py
@@ -17,10 +17,16 @@ mydb = myclient["UExchange"]
 accounts = mydb["Accounts"]
 requests = mydb["Requests"]
 
-testAccount = {"_id": "0" , "name" : "Tester", "phone" : "555-555-5555", "email" : "school@edu.edu", "password" : "password",}
-testRequest = {"_id": "0", "title" : "REQUEST", "subtitle" : "This is a request for xyz", "assignedUser" : "0", "pickedUpUser": "TBD", "pickedUp" : False}
+# testAccount = {"_id": "0" , "name" : "Tester", "phone" : "555-555-5555", "email" : "school@edu.edu", "password" : "password",}
+# testRequest = {"_id": "0", "title" : "REQUEST", "subtitle" : "This is a request for xyz", "assignedUser" : "0", "pickedUpUser": "TBD", "pickedUp" : False}
 
-x = accounts.insert_one(testAccount)
-y = requests.insert_one(testRequest)
-print(x.inserted_id)
-print(y.inserted_id)
+# x = accounts.insert_one(testAccount)
+# y = requests.insert_one(testRequest)
+# print(x.inserted_id)
+# print(y.inserted_id)
+
+
+@socketio.on('_testConnection')
+def test_connection(message):
+	print("User connected")
+	send("You have connected!")
