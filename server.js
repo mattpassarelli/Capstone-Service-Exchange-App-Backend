176e8802e989d01f0bf9249ae81a4e5e7f1173ed
Matt Passarelli <mattvpassarelli@gmail.com>
2019-01-27 18:36:05 -0500

    oh no

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const server = require('http').createServer().listen(3000);
+const io = require('socket.io')(server);
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+var count = 0;
 
-var connCounter = 0;
+io.on("connect", (socket) =>
+{
+	console.log("User connected " + count);
+	count++;
 
-const server = express()
-	.use((req, res) => res.sendFile(INDEX))
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-
-io.on('connect', (socket) => {
-	connCounter++;
-	console.log('Client connected');
-	console.log(connCounter)
-	socket.on('disconnect', () => {
-		console.log('Client disconnected')
-		connCounter--;
-		console.log(connCounter)
-	});
-});
-
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+	socket.on("disconnect", () =>
+	{
+		console.log("User disconnected")
+	})
+})
