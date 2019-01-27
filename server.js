184099458323a461fe0b101a64e623ca9721ff41
Matt Passarelli <mattvpassarelli@gmail.com>
2019-01-27 13:15:20 -0500

    remove that lol. Added log output for number of users

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
+var connCounter = 0;
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+	.use((req, res) => res.sendFile(INDEX))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  const io = socketIO(server);
+const io = socketIO(server);
 
 
-  io.on('connect', (socket) => {
+io.on('connect', (socket) => {
+	connCounter++;
 	console.log('Client connected');
-	socket.on('disconnect', () => console.log('Client disconnected'));
-  });
+	console.log(connCounter)
+	socket.on('disconnect', () => {
+		console.log('Client disconnected')
+		connCounter--;
+		console.log(connCounter)
+	});
+});
 
   //setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-
-  io.on("test", (data) => {
-	  console.log(data)
-  })
