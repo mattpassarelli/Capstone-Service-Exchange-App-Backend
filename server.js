d995daa2c35a18098fe4fc4bdf2d5d100de8ebfa
Matt Passarelli <mattvpassarelli@gmail.com>
2019-02-12 16:30:16 -0500

    Server now send confirmation email upon verification. Checks for duplicated email for users. Changed creation email

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ var transporter = nodemailer.createTransport({
 })
 
 
+/**
+ * TODO: 
+ * Actually write email for verfiication code
+ * Write a confirmation email
+ */
+
 mongoose.connect('mongodb+srv://mattpassarelli:Barisax24@uexchange-db-7skbv.mongodb.net/uexchange?retryWrites=true', { useNewUrlParser: true })
 
 var count = 0;
@@ -33,6 +39,8 @@ var accountSchema = new mongoose.Schema({
 })
 
 //Scehma for the request Models
+//TODO: I could not rely on an array of requests per user, instead having a visible boolean for the requests that determine if
+//they get loaded or not
 var requestSchema = new mongoose.Schema({
 	//TODO design and implement request requirements
 	title: 'string',
@@ -63,6 +71,12 @@ io.on("connect", (socket) => {
 
 	//takes Request data from frontend and adds to database
 	socket.on("saveRequest", (data) => {
+		/**
+		 * TODO: 
+		 * 
+		 * Save request to User Account. Either by email or ID
+		 */
+
 		var newRequest = new Request({ title: data.title, subtitle: data.subtitle, posterID: data.posterID })
 
 		/**
@@ -102,34 +116,47 @@ io.on("connect", (socket) => {
 		var salt = bcrypt.genSaltSync(10)
 		var hash = bcrypt.hashSync(data.password, salt)
 
+		var verificationEmail = ('Hello there, ' + data.firstName + " " + data.lastName + ',' + "<p>&nbsp; Your verification code is </p> + <p>&nbsp; Please go back to the app and type in this code to verify your account and login.</p> <p>&nbsp;Thank you,</p>	<p>&nbsp;&nbsp; UxEchange creator Matt</p>")
 
-
-		var newUser = new Account({
-			firstName: data.firstName, lastName: data.lastName,
-			email: data.email, phoneNumber: data.phoneNumber, password: hash, verificationCode: verCode
-		})
-
-		const mailOptions = {
-			from: 'app.uexchange@gmail.com',
-			to: newUser.email,
-			subject: "Hello there!",
-			html: ('<p>Hello there. General Kenobi. Your verification code is </p>' + verCode)
-		}
-
-		console.log("New user data: " + newUser)
-
-		transporter.sendMail(mailOptions, function (err, info) {
+		Account.findOne({ email: data.email }, function (err, doc) {
 			if (err) { console.log(err) }
-
-			else { console.log(info) }
-		})
-
-		console.log("Email should have been sent")
-
-		newUser.save(function (err, account) {
-			if (err) { console.log(err) }
+			else if (doc) {
+				console.log("Account found. Duplicate email: " + doc)
+				socket.emit("creationReturn", ("Email Already Used"))
+			}
 			else {
-				console.log("Account has been added. Awaiting verification for user: " + account.email)
+				var newUser = new Account({
+					firstName: data.firstName, lastName: data.lastName,
+					email: data.email, phoneNumber: data.phoneNumber, password: hash, verificationCode: verCode
+				})
+
+				const verificationEmailOptions = {
+					from: 'app.uexchange@gmail.com',
+					to: newUser.email,
+					subject: "Hello there! UxEchange Account Verification",
+					html: (verificationEmail + verCode)
+				}
+
+				
+
+				console.log("New user data: " + newUser)
+
+				transporter.sendMail(verificationEmailOptions, function (err, info) {
+					if (err) { console.log(err) }
+
+					else { console.log(info) }
+				})
+
+				console.log("Email should have been sent")
+
+				newUser.save(function (err, account) {
+					if (err) { console.log(err) }
+					else {
+						console.log("Account has been added. Awaiting verification for user: " + account.email)
+						
+						socket.emit("creationReturn", ("Email Not Used"))
+					}
+				})
 			}
 		})
 	})
@@ -141,6 +168,9 @@ io.on("connect", (socket) => {
 		 */
 
 		console.log(data)
+
+		var confirmationEmail = ("Hello there, " + data.firstName + " " + data.lastName + "," + "<p>&nbsp; Your account with UxEchange has been verified! You may now log into the app. </p> <p>&nbsp;Thank you,</p> <p>&nbsp;&nbsp; UxEchange creator Matt</p>")
+
 
 		Account.findOne({ email: data.email }, function (err, docs) {
 			if (err) { console.log(err) }
@@ -168,9 +198,33 @@ io.on("connect", (socket) => {
 					rtnMessage = "Codes do not match"
 				}
 				console.log(rtnMessage)
+
+				const confirmationEmailOptions = {
+					from: 'app.uexchange@gmail.com',
+					to: newUser.email,
+					subject: "Hello there! UxEchange Account Confirmation",
+					html: (confirmationEmail)
+				}
+
+				transporter.sendMail(confirmationEmailOptions, function(err, info){
+					if(err){console.log(err)}
+					else{
+						console.log(info)
+					}
+				})
 				socket.emit("isAccountVerified", (rtnMessage))
 			}
 		})
+	})
+
+	socket.on("requestLogin", (data) => {
+
+		/**
+		 * TODO: 
+		 * 
+		 * Find by email, check passwords match -> return code -> let client process login
+		 */
+		console.log(data)
 	})
 })
 
