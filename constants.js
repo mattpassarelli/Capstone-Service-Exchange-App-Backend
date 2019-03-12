cad26b1c8970b172632bbc765dd92535487e9cd0
Matt Passarelli <mattvpassarelli@gmail.com>
2019-03-12 15:18:24 -0400

    So it turns out herokualso needs to see my constants file. Turns out it can't. I need to figure something else out

---
+++ constants.js
@@ -0,0 +1,9 @@
+const MONGO_URL = 'mongodb+srv://mattpassarelli:Barisax24@uexchange-db-7skbv.mongodb.net/uexchange?retryWrites=true'
+const APP_EMAIL = "app.uexchange@gmail.com"
+const APP_EMAIL_PASSWORD = "qiL9rY!*Hwsj"
+
+module.exports ={
+    MONGO_URL: MONGO_URL,
+    APP_EMAIL: APP_EMAIL,
+    APP_EMAIL_PASSWORD: APP_EMAIL_PASSWORD
+}
