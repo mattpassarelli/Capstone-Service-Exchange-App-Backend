3bc42706dcf33d773bc0c3ac819835b16ae2b604
Matt Passarelli <19616900+mattpassarelli@users.noreply.github.com>
2019-03-12 13:51:32 -0400

    Delete constants.js

--- constants.js
+++
@@ -1,3 +0,0 @@
-export const MONGO_URL = 'mongodb+srv://mattpassarelli:Barisax24@uexchange-db-7skbv.mongodb.net/uexchange?retryWrites=true'
-export const APP_EMAIL = "app.uexchange@gmail.com"
-export const APP_EMAIL_PASSWORD = "qiL9rY!*Hwsj"
